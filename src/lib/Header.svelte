917d72d023382fb078c936ca8d2e1d0e30d49683
Teemu Sairi <ag3144@student.jamk.fi>
2025-04-30 10:03:29 +0300

    Korjattu teksti 'Add notes', korjattu pisteet Ingredients kohdassa, poistettu ylimääräistä koodia

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
 </script>
 
-<header class="flex h-30 items-center justify-center rounded-xl border-b-2 bg-rose-50">
+<header class="flex h-30 items-center justify-center rounded-xl border-b-2 bg-rose-100">
 	<img src="logo-v-1.png" alt="logo" />
 </header>
-
-<style>
-	img {
-		width: 300px;
-	}
-</style>
