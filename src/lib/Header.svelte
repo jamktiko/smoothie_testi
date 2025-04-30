32c5d7e2dee8874388a10fc777573a92da77aaaf
Lauri Makkonen <ag4039@student.jamk.fi>
2025-04-30 09:22:51 +0300

    logo pienempi

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,11 +2,11 @@
 </script>
 
 <header class="flex h-30 items-center justify-center rounded-xl border-b-2 bg-rose-50">
-	<img src="logo-v-1.png" alt="logo" />
+	<img src="logo-v-2.png" alt="logo" />
 </header>
 
 <style>
 	img {
-		width: 300px;
+		width: 250px;
 	}
 </style>
