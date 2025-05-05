541858e786a088cfd7796db36b3f7722886efafb
Lauri Makkonen <ag4039@student.jamk.fi>
2025-05-05 14:08:42 +0300

    onclick notesista pois

--- src/lib/Notes.svelte
+++ src/lib/Notes.svelte
@@ -8,5 +8,4 @@
 	} = $props();
 </script>
 
-<textarea class="w-full resize-none" on:click|stopPropagation bind:value={taytto} {placeholder}
-></textarea>
+<textarea class="w-full resize-none" bind:value={taytto} {placeholder}></textarea>
