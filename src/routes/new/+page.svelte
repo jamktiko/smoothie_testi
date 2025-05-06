085d69ecad897976959295ab7a00766cb11bd7da
Janika,Rahikainen <ag3081@student.jamk.fi>
2025-05-06 11:58:19 +0300

    uuden sivun päivitys

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,34 +1,51 @@
 <script lang="ts">
+	// ----------------------- IMPORTIT ---------------------------
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import type { Fruit } from '$lib/types/fruit';
+
+	// ----------------------- MUUTTUJAT --------------------------
+	//hedelmien määrät, null tarkoittaa että ei ole valittu mitään
+	type Amount = 1 | 2 | 3 | 4 | 5 | null;
+	//Valittu määrä tallennetaan tähän
+	let amount: Amount = $state(null);
+
+	let fruits: Fruit[] = $state([]);
+	// Valittu hedelmä tallennetaan tähän
+	let selected: string = $state('');
+	// ----------------------- FUNKTIOT ---------------------------
+	//button mikä vie etusivulle
 	function homePage() {
 		goto('/');
 	}
-
-	import { onMount } from 'svelte';
-	let fruits: Fruit[] = $state([]);
-
 	// Hakee hedelmät fruits.json tiedostosta
 	onMount(async () => {
 		const response = await fetch('/data/fruits.json');
 		fruits = await response.json();
 	});
-
-	// Valitut asiat tallennetaan tähän
-	let selected: string[] = $state([]);
-	$inspect(selected);
-	// Funktio valinnan käsittelemiseen
 </script>
 
-<button id="homepage" onclick={homePage}>Home</button>
 <h1>Add new recipe</h1>
-<h2>Valitse hedelmiä:</h2>
-<select bind:value={selected}>
-	{#each fruits as fruit}
-		<option value={fruit.name}>{fruit.name}</option>
-	{/each}
-</select>
-<p>{selected}</p>
+<div>
+	<!-- Tästä valitaan raaka-aineen määrä -->
+	<select bind:value={amount}>
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+	</select>
+	<!-- Tästä valitaan raaka-aine -->
+	<select bind:value={selected}>
+		{#each fruits as fruit}
+			<option value={fruit.name}>{fruit.name}</option>
+		{/each}
+	</select>
+
+	<p>{amount} {selected}</p>
+
+	<button id="homepage" onclick={homePage}>Home</button>
+</div>
 
 <style>
 	#homepage {
