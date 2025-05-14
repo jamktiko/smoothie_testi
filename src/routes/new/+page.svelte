9e4bab6955468da6562361f236c6250ce00486ad
Teemu Sairi <ag3144@student.jamk.fi>
2025-05-14 11:57:48 +0300

    Vain tarvittava material symbols iconit käytössä

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -192,17 +192,6 @@
 
 <!-- ----------------------- HTML --------------------------- -->
 
-<link
-	rel="stylesheet"
-	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-/>
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" />
-<link
-	href="https://fonts.googleapis.com/css2?family=Honk&family=Kalnia+Glaze:wght@100..700&family=Laila:wght@300;400;500;600;700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Merienda:wght@300..900&family=Nabla&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-	rel="stylesheet"
-/>
-
 <!-- Footer Fix Div -->
 <div class="h-auto min-h-screen">
 	<!-- Outer Div -->
